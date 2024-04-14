840703f160c2d7b93e76318c0e35e5459f301262
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:15:54 +0200

    # Добавлен IRepository and BookRepository

---
+++ GuestBookRasorPagesRepository/Repository/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using GuestBookRasorPagesRepository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuestBookRasorPagesRepository.Repository
+{
+    public class BookRepository: IRepository
+    {
+        private readonly ClassContext _context;
+
+        public BookRepository(ClassContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Messages>> GetMessageList()
+        {            
+            return await _context.Messages.ToListAsync();
+        }
+
+        public async Task<Messages> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public async Task Create(Messages c)
+        {
+            await _context.Messages.AddAsync(c);
+        }
+
+        public async Task<List<User>> GetUserList()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUser(string login)
+        {
+            return _context.Users.Where(m => m.Login == login).First();
+        }
+
+        public async Task CreateUs(User u)
+        {
+            await _context.Users.AddAsync(u);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ GuestBookRasorPagesRepository/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using GuestBookRasorPagesRepository.Models;
+
+namespace GuestBookRasorPagesRepository.Repository
+{
+    public interface IRepository
+    {
+        Task<List<Messages>> GetMessageList();
+        Task<Messages> GetMessage(int id);
+        Task Create(Messages item);
+        Task<List<User>> GetUserList();
+        Task<User> GetUser(string login);
+        Task CreateUs(User item);
+        Task Save();
+    }
+}
