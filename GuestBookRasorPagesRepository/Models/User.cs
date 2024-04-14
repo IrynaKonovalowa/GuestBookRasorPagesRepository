68dc7b82da46dd04185b7b1e2f75fe1bd2ad5fda
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:10:32 +0200

    # Добавлен класс User

---
+++ GuestBookRasorPagesRepository/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace GuestBookRasorPagesRepository.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Messages = new HashSet<Messages>();
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+
+        public string? Salt { get; set; }
+
+        virtual public ICollection<Messages>? Messages { get; set; }
+    }
+}
