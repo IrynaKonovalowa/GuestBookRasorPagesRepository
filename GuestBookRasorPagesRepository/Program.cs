87e8f7552b7987ef181b4fc1f73f150bc76b14c6
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:05:51 +0200

    #  Изменен класс Programm

--- GuestBookRasorPagesRepository/Program.cs
+++ GuestBookRasorPagesRepository/Program.cs
@@ -1,7 +1,19 @@
+using GuestBookRasorPagesRepository.Models;
+using GuestBookRasorPagesRepository.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ClassContext>(options => options.UseLazyLoadingProxies().UseSqlServer(connection));
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IRepository, BookRepository>();
 
 var app = builder.Build();
 
@@ -12,6 +24,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
