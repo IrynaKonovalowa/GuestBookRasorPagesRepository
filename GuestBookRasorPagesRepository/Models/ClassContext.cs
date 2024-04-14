424cd45dfcc18ac498f97610de1d990457475181
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:08:32 +0200

    # Добавлен класс Context

---
+++ GuestBookRasorPagesRepository/Models/ClassContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuestBookRasorPagesRepository.Models
+{
+    public class ClassContext: DbContext
+    {
+         public DbSet<Messages> Messages { get; set; }
+         public DbSet<User> Users { get; set; }
+         public ClassContext(DbContextOptions<ClassContext> options)
+             : base(options)
+         {
+            if (Database.EnsureCreated())
+            {
+                User user = new User();
+                user.Login = "Tom";
+                Messages?.Add(new Messages { Message="Very good hotel!", MessageDate = DateTime.Now, User=user });
+                SaveChanges();
+            }
+         }       
+    }
+}
