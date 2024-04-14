d381094babe2cce7ea164d061feb35204481826e
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:09:06 +0200

    # Добавлена LoginViewModel

---
+++ GuestBookRasorPagesRepository/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBookRasorPagesRepository.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Field must be set")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Field must be set")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
