77ebbc2f3305123975e3df7bdd3ee87563bb4bfa
IrynaKonovalowa <iri.moummi@gmail.com>
2024-04-14 16:09:44 +0200

    # Добавлен класс  Messages

---
+++ GuestBookRasorPagesRepository/Models/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBookRasorPagesRepository.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Review")]
+        [Required(ErrorMessage = "Field must be set")]
+        public string? Message { get; set; }
+        [Display(Name = "Date")]
+        public DateTime? MessageDate { get; set; }
+
+        virtual public User? User { get; set; }
+    }
+}
